eca446ab317f3a8cd7d8aa749f84b1a920e0f1be
raymond zhao <raymondzhao158@gmail.com>
2021-04-05 13:49:36 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(visualFlare = "*"){
+
+    return function(x="special"){
+        return `You are ${visualFlare}${x}${visualFlare}!`
+    }
+    const encouragingPromptFunction = wrapAdjective("!!!")
+}
+
+wrapAdjective("%")("a dedicated programmer");
